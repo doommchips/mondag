204f381e562c5cf7109559499a604afdf487c944
doommchips <dgtl.bw@gmail.com>
2023-10-24 23:14:28 +0200

    add vercel analytics

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -1,6 +1,10 @@
 import { getArtists } from "$lib/utils/sanity";
 import { getExhibitions } from "$lib/utils/sanity";
 import { error } from "@sveltejs/kit";
+import { dev } from '$app/environment';
+import { inject } from '@vercel/analytics';
+ 
+inject({ mode: dev ? 'development' : 'production' });
 
 export const load = async () => {
   const artists = await getArtists();
